2c9476f96ab6777d037c1e8ce8b2a9d14929f269
mgalliou <matthias.galliou@gmail.com>
2021-02-16 15:39:49 +0100

    comment unused sockopt; setup ttl and recvtimeout sockopt

--- src/setup_socket.c
+++ src/setup_socket.c
@@ -6,7 +6,7 @@
 /*   By: mgalliou <mgalliou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/21 12:41:00 by mgalliou          #+#    #+#             */
-/*   Updated: 2021/02/16 10:47:27 by mgalliou         ###   ########.fr       */
+/*   Updated: 2021/02/16 15:33:47 by mgalliou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,30 @@
 int setup_socket(struct addrinfo *res)
 {
 	int             sockfd;
+	int				ttl_val;
+	struct timeval	tv_out; 
 
 	sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
 	g_p.sockfd = sockfd;
 	if (0 > sockfd)
 	{
-		fprintf(stderr, "socket: %d\n", sockfd);
-		perror(NULL);
-		return (2);
+		fprintf(stderr, "Failed to open socket: %d\n", sockfd);
+		return (-1);
 	}
+	ttl_val = 128;
+	if (0 > setsockopt(sockfd, SOL_IP, IP_TTL, &ttl_val, sizeof(ttl_val))) 
+	{ 
+		fprintf(stderr, "Failed to set ttl sockopt.\n"); 
+		return (-1); 
+	} 
+	tv_out.tv_sec = 1; 
+	tv_out.tv_usec = 0;
+	if (0 > setsockopt(sockfd, SOL_SOCKET, SO_RCVTIMEO, (const char*)&tv_out, sizeof tv_out))
+	{
+		fprintf(stderr, "Failed to set recvtimeout sockopt.\n"); 
+		return (-1); 
+	} 
+	/*
 	g_p.sokbuf_size = IP_MAXPACKET + 128;
 	if (0 > (setsockopt(sockfd, SOL_SOCKET, SO_RCVBUF,
 					(int*)&g_p.sokbuf_size, sizeof(int))))
@@ -39,6 +54,7 @@ int setup_socket(struct addrinfo *res)
 		perror(NULL);
 		return (2);
 	}
+	*/
 	return (0);
 }
 
